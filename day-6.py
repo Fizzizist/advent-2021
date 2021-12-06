63e0a6066f8c09eb02610c23cf329eb742154334
Peter Vlasveld <peter.vlasveld@cyclicarx.com>
2021-12-06 14:06:18 +0000

    added day 6

---
+++ day-6.py
@@ -0,0 +1,21 @@
+import sys
+from tqdm import tqdm
+
+
+def main():
+    fish_ages = [0,0,0,0,0,0,0,0,0]
+    with open(sys.argv[1]) as f:
+        for num in f.readline().split(','):
+            fish_ages[int(num)] += 1
+
+
+    for _ in tqdm(range(int(sys.argv[2]))):
+        babies = fish_ages[0]
+        for i in range(8):
+            fish_ages[i] = fish_ages[i+1]
+        fish_ages[8] = babies
+        fish_ages[6] += babies
+
+    print(sum(fish_ages))
+
+main()
